14220414aafb1c2570faafdbf3ef622236ba6390
believer <www.koushiksaha.master@gmail.com>
2023-09-16 10:33:15 +0600

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
       </div>
 
       <div className="container my-3">
-        <TextFrom heading="Enter the text to Analyze Blow" />
+        <TextFrom />
+        //heading="Enter Your Text"
       </div>
 
     </>
--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -22,11 +22,35 @@ export default function TextFrom(props) {
   const [text, setText] = useState('');
 
   return (
-  <div className="mb-3">
-    <h1>{props.heading}</h1>
-    <textarea className="form-control my-5 bg-dark text-light" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
-    <button className="btn btn-info" onClick={handleUpClick}>Convert to Uppercase</button>
-    <button className="btn btn-info" onClick={handleDownClick}>Convert to LowerCase</button>
-  </div>
+    <>
+      <div className="container-fluid p-0 from-box">
+
+        <div className="mb-3">
+          <h1 className='text-light'>{props.heading}</h1>
+          <textarea className="form-control my-5 bg-dark text-light " id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+        </div>
+
+        <div className="col">
+          <h1 className='text-light'>Your text summary</h1>
+          <h4 className='text-light'>{text.split(" ").length} words <br/>{text.length} characters</h4>
+          <h4 className='text-light'>{0.008 * text.split(" ").length} minutes to read</h4>
+        </div>
+
+      </div>
+
+      <div className='button'>
+
+        <button className="btn btn-info" onClick={handleUpClick}>Convert to Uppercase</button>
+
+        <button className="btn btn-info" onClick={handleDownClick}>Convert to LowerCase</button>
+        
+      </div>
+
+      <div className='preview'>
+        <h1 className='text-light'>Preview</h1>
+        <p className='text-light'>{text}</p>
+      </div>
+
+    </>
   )
 }
